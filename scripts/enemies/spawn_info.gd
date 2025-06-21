2e60b004218e9ef15b71e5fe5892885ec26bab94
S-Sphere <s0.miguel.santos@gmail.com>
2025-06-21 13:54:44 +0100

    added boss animations

--- scripts/enemies/spawn_info.gd
+++ scripts/enemies/spawn_info.gd
@@ -4,7 +4,7 @@ class_name SpawnInfo
 
 @export var time_start			: int
 @export var time_end			: int
-@export var enemy				: Resource
+@export var enemy				: PackedScene
 @export var enemy_number		: int
 @export var enemy_spawn_delay	: int
 
